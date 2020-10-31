2e4e580296de72f8b4157e76e5c0a18add2334c9
James Carlson <jxxcarlson@gmail.com>
2020-10-31 05:45:46 -0400

    Use Data.Text.Lazy.pack for packed strings

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,9 @@ module Main where
 import Lib
 
 import Web.Scotty
+import qualified Data.Text.Lazy as T
+
 
 main = scotty 3000 $ do
   get "/" $ do
-    html "Hello World!"
+    html .  T.pack $ "Hello World!"
