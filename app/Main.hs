90c30789094e0914c8c7ffda92924a327fb9b7be
James Carlson <jxxcarlson@gmail.com>
2020-10-31 05:52:06 -0400

    Add logging with Network.Wai.Middleware.RequestLogger

--- app/Main.hs
+++ app/Main.hs
@@ -5,9 +5,12 @@ module Main where
 import Lib
 
 import Web.Scotty
+import Network.Wai.Middleware.RequestLogger
 import qualified Data.Text.Lazy as T
 
 
 main = scotty 3000 $ do
+  middleware logStdoutDev
+  
   get "/" $ do
     html .  T.pack $ "Hello World!"
