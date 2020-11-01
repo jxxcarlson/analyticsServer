115804213bd16e3a64b75883aac999c690aa5af1
James Carlson <jxxcarlson@gmail.com>
2020-11-01 01:32:59 -0400

    make separate: dbStart and dbInsert

--- src/Entity.hs
+++ src/Entity.hs
@@ -13,13 +13,14 @@
 {-# LANGUAGE TypeFamilies               #-}
 
 
-module Entity where
+module Entity (dbStart, dbInsert) where
 
 import           Control.Monad.IO.Class  (liftIO)
 import           Database.Persist
 import           Database.Persist.Postgresql
 import           Database.Persist.TH
 import           Control.Monad.Logger    (runStderrLoggingT)
+import qualified Data.Text.Lazy as T
 
 import Event
 
@@ -33,14 +34,37 @@ EventEntity
     deriving Show
 |]
 
-dbFunction :: IO ()
-dbFunction = runStderrLoggingT $ withPostgresqlPool connStr 10 $ \pool ->
+dbStart :: IO ()
+dbStart = runStderrLoggingT $ withPostgresqlPool connStr 10 $ \pool ->
     liftIO $ do
         flip runSqlPersistMPool pool $ do
+
             runMigration migrateAll
 
             insert $ EventEntity "jxxcarlson"  "signin" 1234.22
             insert $ EventEntity "mary88"  "signin" 1260.19
 
-            liftIO  $ putStrLn "Done"
+            liftIO  $ putStrLn "\nDatabase up and running\n"
+
+dbInsert :: Event -> IO ()
+dbInsert event = runStderrLoggingT $ withPostgresqlPool connStr 10 $ \pool ->
+    liftIO $ do
+        flip runSqlPersistMPool pool $ do
+            insert $ EventEntity (T.unpack $ userName event)  (T.unpack $ eventName event) (eventTime event)
+            liftIO  $ putStrLn $ "Inserted " ++ (T.unpack $ eventName event)
+
+
+-- inHandlerDb = liftIO . dbFunction
+
+-- inAppDb = liftIO . dbFunction
+
+-- dbFunction query = runStderrLoggingT $ 
+--         withPostgresqlPool connStr 10 $ 
+--         \pool -> liftIO $ runSqlPersistMPool query pool
+
+-- doMigrations = runMigration migrateAll
+
+-- dbInitialData = do
+--     insert $ EventEntity "jxxcarlson"  "signin" 1234.22
+--     insert $ EventEntity "mary88"  "signin" 1260.19
 
